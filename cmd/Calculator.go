2b13f4274b9318dc5bb4304314b899af99dde6a4
nbaybara <nsbaybar@gmail.com>
2022-04-23 01:58:57 +0300

    updated calculation of avergae

--- cmd/Calculator.go
+++ cmd/Calculator.go
@@ -1,5 +1,10 @@
 package cmd
 
+import (
+	"fmt"
+	"os"
+)
+
 func calculateAverage(numbers []int) int {
 
 	totalLength := len(numbers)
@@ -8,26 +13,39 @@ func calculateAverage(numbers []int) int {
 	for i := 0; i < totalLength; i++ {
 		totalScore += numbers[i]
 	}
-
 	return totalScore / totalLength
-
 }
 
 func calculatePercentageOfScore(userScore int, scores []int) int {
 
 	worstQuizer := 0
-	totalLength := len(scores)
+	scoreEliminated := removeDuplicate(scores)
+	totalLength := len(scoreEliminated)
 
 	if totalLength == 1 {
 		return 100
 	}
 
+	fmt.Fprintf(os.Stdout, " totalLength (%d )\n", userScore)
+
 	for i := 0; i < totalLength; i++ {
 
-		if userScore < scores[i] {
+		if userScore > scores[i] {
 			worstQuizer++
 		}
 	}
 
 	return worstQuizer * 100 / totalLength
+}
+
+func removeDuplicate(scores []int) []int {
+	m := make(map[int]int)
+	for _, x := range scores {
+		m[x] = x
+	}
+	var ClearedArr []int
+	for x, _ := range m {
+		ClearedArr = append(ClearedArr, x)
+	}
+	return ClearedArr
 }
